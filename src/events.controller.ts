7f410a8e2fff94f0f257c20085b1cf8189781550
alfinkerin <kerinalfin70@gmail.com>
2023-11-21 01:45:57 +0700

    data validation

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -6,11 +6,12 @@ import {
   Get,
   HttpCode,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Like, MoreThan, Repository } from 'typeorm';
 import { CreateEventDto } from './create-event.dto';
 import { Event } from './event.entity';
 import { UpdateEventDto } from './update-event.dto';
@@ -27,9 +28,30 @@ export class EventsController {
     return await this.repository.find();
   }
 
+  @Get('/practice')
+  async practice() {
+    return await this.repository.find({
+      select: ['id', 'when'],
+      where: [
+        {
+          id: MoreThan(3),
+          when: MoreThan(new Date('2021-02-12T13:00:00')),
+        },
+        {
+          description: Like('%meet%'),
+        },
+      ],
+      take: 2,
+      order: {
+        id: 'DESC',
+      },
+    });
+  }
+
   @Get(':id')
-  async findOne(@Param('id') id) {
-    return await this.repository.findOne(id);
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    console.log(typeof id);
+    return await this.repository.findOneBy({ id: id });
   }
 
   @Post()
