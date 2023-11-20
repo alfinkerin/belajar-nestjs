0e3088b95cbf0ba9cf9c8fc3361e4bb7f1a8bef8
alfinkerin <kerinalfin70@gmail.com>
2023-11-20 08:17:04 +0700

    add docker , add typeOrm

---
+++ src/event.entity.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Event {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  when: Date;
+
+  @Column()
+  address: string;
+}
